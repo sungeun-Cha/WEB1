cc1b3d02da753d608554861c4cd750a204474814
sungeunCha <cseun999@naver.com>
2018-10-05 20:47:02 +0900

    2018.10.05_JS 및Jquery실습

---
+++ colors.js
@@ -0,0 +1,36 @@
+  var Links = {
+    setColor : function(color){
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length){
+        //     alist[i].style.color = color;
+        //     i = i + 1;
+        // }
+        $('a').css('color', color) //모든 a태그를 제이쿼리로 제어하겠다는 의미
+     }
+  }
+  var Body = {
+    setColor : function(color){
+      //document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+    },
+    setBackgroundColor : function(color){
+      //document.querySelector('body').style.backgroundColor= color;
+        $('body').css('backgroundColor', color);
+    }
+  }
+
+  function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      self.value = 'day';
+      Links.setColor('white');
+    }else{
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      self.value = 'night';
+      Links.setColor('blue');
+    }
+  }
